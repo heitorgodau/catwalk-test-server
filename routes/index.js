2b4bbcf891d77deb346ab7d877caf9355624df1e
Heitor <heitorgodau@gmail.com>
2019-08-29 20:55:39 -0300

    promises refactored to async await

--- routes/index.js
+++ routes/index.js
@@ -5,83 +5,75 @@ const Event = require('../models/Event');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.status(200).json('ok');
+  res.status(200).json('API is working');
 });
 
 
 // SEARCH all events
-router.get('/events', (req, res) => {
-  Event.find()
-    .then((result) => {
-      res.status(200).json(result);
-    })
-    .catch((err) => {
-      res.json(err);
-      throw new Error(err);
-    });
+router.get('/events', async (req, res) => {
+  try {
+    const result = await Event.find();
+    res.status(200).json(result);
+  } catch (err) {
+    throw new Error(err);
+  }
 });
 
 // SEARCH a specific event
-router.get('/event/:eventId', (req, res) => {
+router.get('/event/:eventId', async (req, res) => {
   if (!mongoose.Types.ObjectId.isValid(req.params.eventId)) {
     res.status(400).json({ message: 'Specified ID is not valid' });
     return;
   }
   const { eventId } = req.params;
-  Event.findById(eventId)
-    .then((response) => {
-      res.status(200).json(response);
-    })
-    .catch((err) => {
-      res.json(err);
-      throw new Error(err);
-    });
+  try {
+    const result = await Event.findById(eventId);
+    res.status(200).json(result);
+  } catch (err) {
+    throw new Error(err);
+  }
 });
 
 // EDIT a specific event
-router.put('/event/:eventId', (req, res) => {
+router.put('/event/:eventId', async (req, res) => {
   if (!mongoose.Types.ObjectId.isValid(req.params.eventId)) {
     res.status(400).json({ message: 'Specified ID is not valid' });
     return;
   }
   const { eventId } = req.params;
-  Event.findByIdAndUpdate(eventId, req.body)
-    .then(() => {
-      res.json({ message: `Event with ${req.params.id} ID is updated successfully.` });
-    })
-    .catch((err) => {
-      res.json(err);
-      throw new Error(err);
-    });
+  try {
+    await Event.findByIdAndUpdate(eventId, req.body);
+    res.json({ message: `Event with ${req.params.id} ID is updated successfully.` });
+  } catch (err) {
+    throw new Error(err);
+  }
 });
 
 // CREATE a new event
-router.post('/create-event', (req, res) => {
+router.post('/create-event', async (req, res) => {
   if (!req.body.imageUrl) req.body.imageUrl = 'https://aliceasmartialarts.com/wp-content/uploads/2017/04/default-image.jpg';
-  Event.create(req.body)
-    .then(result => res.status(200).json(result))
-    .catch((err) => {
-      res.json(err);
-      throw new Error(err);
-    });
+  try {
+    const result = await Event.create(req.body);
+    res.status(200).json(result);
+  } catch (error) {
+    throw new Error(error);
+  }
 });
 
 
 // DELETE a specific event
-router.get('/event/delete/:eventId', (req, res) => {
+router.get('/event/delete/:eventId', async (req, res) => {
   if (!mongoose.Types.ObjectId.isValid(req.params.eventId)) {
     res.status(400).json({ message: 'Specified ID is not valid' });
     return;
   }
   const { eventId } = req.params;
-  Event.findByIdAndDelete(eventId)
-    .then(() => {
-      res.json({ message: `Event with ${req.params.eventId} ID is delete successfully.` });
-    })
-    .catch((err) => {
-      res.json(err);
-      throw new Error(err);
-    });
+  try {
+    await Event.findByIdAndDelete(eventId)
+    res.json({ message: `Event with ${req.params.eventId} ID is delete successfully.` });
+  } catch (err) {
+    throw new Error(err);
+  }
 });
 
 module.exports = router;
